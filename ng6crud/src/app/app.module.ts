aa6dec09127df7d07ea5385af0573e51b4d04d97
ricardo.ara.silva@gmail.com <ricardo.ara.silva@gmail.com>
2018-09-04 11:11:00 +0100

    third commit

--- ng6crud/src/app/app.module.ts
+++ ng6crud/src/app/app.module.ts
@@ -2,13 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CreateComponent } from './components/create/create.component';
+import { IndexComponent } from './components/index/index.component';
+import { EditComponent } from './components/edit/edit.component';
+
+import {RouterModule, Routes} from '@angular/router';
+
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
+
+const routes: Routes = [
+  {
+    path: 'create',
+    component: CreateComponent
+  },
+  {
+    path: 'edit/:id',
+    component: EditComponent
+  },
+  {
+    path: 'index',
+    component: IndexComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateComponent,
+    IndexComponent,
+    EditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    SlimLoadingBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
